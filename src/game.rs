777a8a2b5a1dee164ffecc0db7ac23e503a055d0
yabernar <mad_otter@protonmail.ch>
2021-03-19 16:23:06 +0100

    Initialisation

---
+++ src/game.rs
@@ -0,0 +1,136 @@
+pub fn new_game() -> (u64, u64){
+    (68853694464, 34628173824)
+}
+/*
+fn put_piece((player : u64, adversary : u64), position:u64) -> (u64, u64){
+    if position & (player | adversary) > 0 { // Position is not already set
+        return (player | position, adversary) 
+    }
+    (0, 0) // Fail state
+}*/
+
+// Bitboards with borders set to 1;
+pub static TOP_BORDER: u64 = 255;
+pub static LEFT_BORDER: u64 = 72340172838076673;
+pub static RIGHT_BORDER: u64 = 9259542123273814144;
+pub static BOTTOM_BORDER: u64 = 18374686479671623680;
+pub static TOPLEFT_BORDER: u64 = TOP_BORDER | LEFT_BORDER;
+pub static TOPRIGHT_BORDER: u64 = TOP_BORDER | RIGHT_BORDER;
+pub static BOTTOMLEFT_BORDER: u64 = BOTTOM_BORDER | LEFT_BORDER;
+pub static BOTTOMRIGHT_BORDER: u64 = BOTTOM_BORDER | RIGHT_BORDER;
+
+pub static mut TOTAL_MOVES: u64 = 0;
+pub static mut EXPLORED_MOVES: u64 = 0;
+
+// Assumes the position sent is a legal move
+pub fn play_move(game :(u64, u64), position:u64) -> (u64, u64){
+    let mut player_next = game.0 | position;
+
+    right_shift_search(TOP_BORDER, game, position, &mut player_next, 8);
+    right_shift_search(LEFT_BORDER, game, position, &mut player_next, 1);
+    left_shift_search(RIGHT_BORDER, game, position, &mut player_next, 1);
+    left_shift_search(BOTTOM_BORDER, game, position, &mut player_next, 8);
+    right_shift_search(TOPLEFT_BORDER, game, position, &mut player_next, 9);
+    right_shift_search(TOPRIGHT_BORDER, game, position, &mut player_next, 7);
+    left_shift_search(BOTTOMLEFT_BORDER, game, position, &mut player_next, 7);
+    left_shift_search(BOTTOMRIGHT_BORDER, game, position, &mut player_next, 9);
+
+    // Checking if some pieces have been flipped
+    if player_next ^ (game.0 | position) == 0 {
+        return (0,0) // Fail state
+    }
+
+    // Changing adversary board
+    let adversary_next = game.1 & (game.1 ^ player_next); // Removing the flipped pieces with a XOR. As they only appear in both bitboards if they are flipped.
+    (player_next, adversary_next)
+}
+
+fn right_shift_search(border : u64, game : (u64, u64), position : u64, player_next : &mut u64, shift_value : u8) {
+    if position & border == 0{ // Played piece is not on the edge
+        let mut current = position >> shift_value;
+        let mut flips = 0;
+        loop{
+            if current & game.0 > 0{ // Piece in searched position is of own color
+                *player_next = *player_next | flips;
+                break;
+            } else if (current & border == 0) && (current & game.1 > 0){ // Piece in searched position is opponent and not on the edge
+                flips = flips | current;
+                current = current >> shift_value;
+                continue;
+            } else { // Either no piece in the searched position, or opponent piece is on the edge
+                break;
+            }
+        }
+    }
+}
+
+fn left_shift_search(border : u64, game : (u64, u64), position : u64, player_next : &mut u64, shift_value : u8) {
+    if position & border == 0{ // Played piece is not on the edge
+        let mut current = position << shift_value;
+        let mut flips = 0;
+        loop{
+            if current & game.0 > 0{ // Piece in searched position is of own color
+                *player_next = *player_next | flips;
+                break;
+            } else if (current & border == 0) && (current & game.1 > 0){ // Piece in searched position is opponent and not on the edge
+                flips = flips | current;
+                current = current << shift_value;
+                continue;
+            } else { // Either no piece in the searched position, or opponent piece is on the edge
+                break;
+            }
+        }
+    }
+}
+
+pub fn create_potential_moves_mask(game : (u64, u64)) -> u64 {
+    let all_pieces = game.0 | game.1;
+    all_pieces ^ ((((game.1 << 1) | (game.1 << 9) | (game.1 >> 7) | LEFT_BORDER) ^ LEFT_BORDER) | 
+                  (((game.1 >> 1) | (game.1 >> 9) | (game.1 << 7) | RIGHT_BORDER) ^ RIGHT_BORDER) |
+                  (game.1 << 8) | (game.1 >> 8) | all_pieces)
+}
+
+pub struct Move {
+    pub game : (u64, u64),
+    pub played_piece: u64,
+    pub next : Vec<Move>,
+}
+
+pub fn process_pseudo_legal_moves(game: (u64, u64), moves_mask: u64) -> Vec<Move>{
+    let mut result : Vec<Move> = Vec::new();
+    let mut start = 1;
+    for _i in 0..64 {
+        if start & moves_mask > 0 {
+            let next_game = reverse(play_move(game, start));
+            if (next_game.0 | next_game.1) > 0{
+                let leaf = Move {
+                    game : next_game,
+                    played_piece : start,
+                    next : Vec::new(),
+                };
+                result.push(leaf);
+            }
+        }
+        start = start << 1;
+    }
+    return result
+}
+
+pub fn depth_search(mut starting_move : Move, depth: u8){
+    let pseudo_legal_moves = create_potential_moves_mask(starting_move.game);
+    let legal_moves = process_pseudo_legal_moves(starting_move.game, pseudo_legal_moves);
+    unsafe {
+        TOTAL_MOVES += legal_moves.len() as u64;
+        EXPLORED_MOVES += 1;
+    }
+    starting_move.next = legal_moves;
+    if depth > 0{
+        for mv in starting_move.next {
+            depth_search(mv, depth-1)
+        }
+    }
+}
+
+pub fn reverse(tuple: (u64, u64)) -> (u64, u64){
+    (tuple.1, tuple.0)
+}
---
+++ src/main.rs
@@ -0,0 +1,85 @@
+mod game;
+use std::io;
+use game::new_game;
+
+fn main() {
+    states_search(9);
+}
+
+fn play_game(){
+    let mut game = new_game();
+    let mut input = String::new();
+    let mut position: u64;
+    for move_nbr in 0..60{
+        print_board(game);
+        if move_nbr % 2 == 0 {
+            let pseudo_legal = game::create_potential_moves_mask(game);
+            let legal_moves = game::process_pseudo_legal_moves(game, pseudo_legal);
+            let mut legal_mask: u64 = 0;
+            for mv in legal_moves{
+                legal_mask = legal_mask | mv.played_piece;
+            }
+            print_board((legal_mask, 0));
+        } else {
+            let pseudo_legal = game::create_potential_moves_mask(game::reverse(game));
+            let legal_moves = game::process_pseudo_legal_moves(game::reverse(game), pseudo_legal);
+            let mut legal_mask: u64 = 0;
+            for mv in legal_moves{
+                legal_mask = legal_mask | mv.played_piece;
+            }
+            print_board((legal_mask, 0));
+        }
+        println!("Enter your next move (5a for instance): ");
+        io::stdin().read_line(&mut input).expect("Failed to read");
+        position = convert(&input);
+        input = "".to_string();
+        if move_nbr % 2 == 0 {
+            game = game::play_move(game, position);
+        } else {
+            game = game::reverse(game::play_move(game::reverse(game), position));
+        }
+    }
+    print_board(game);
+}
+
+fn states_search(depth: u8){
+    let start = game::Move {
+        game : new_game(),
+        played_piece : 0,
+        next : Vec::new(),
+    };
+    use std::time::Instant;
+    let now = Instant::now();
+    game::depth_search(start, depth);
+    let elapsed = now.elapsed();
+    unsafe{
+        println!("--------------------------------------------------------------------\n\tDepth : {}
+                 \n\tExplored : {}\n\tTotal : {}\n\tElapsed : {:.3?}\n\tMoves per second : {}\n\tAverage Moves per position : {}\n", 
+        depth, game::EXPLORED_MOVES, game::TOTAL_MOVES, elapsed, game::TOTAL_MOVES/(elapsed.as_millis() as u64)*1000, game::TOTAL_MOVES/game::EXPLORED_MOVES);
+    }
+}
+
+fn convert(position: &str) -> u64{
+    let column: u8 = position.chars().nth(1).unwrap() as u8 - 97;
+    let line: u8 = position.chars().nth(0).unwrap() as u8 - 49;
+    let offset = line * 8 + column;
+    let pos: u64 = 1<<offset;
+    pos
+}
+
+fn print_board(board:(u64, u64)){
+    for x in 0..8{
+        print!("{}", x+1);
+        for y in 0..8{
+            if (board.0 >> (x*8+y))% 2 == 1 {
+                print!(" X")
+            } else if (board.1 >> (x*8+y))% 2 == 1 {
+                print!(" O")
+            } else {
+                print!{" ."}
+            }
+        }
+        println!("");
+    }
+    println!("  a b c d e f g h");
+}
